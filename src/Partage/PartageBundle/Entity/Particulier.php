f3be474b0847256b65a2aea933f78b7aa773eb5b
Elliot Bouvier <elliotbouvier@yahoo.fr>
2016-06-20 10:43:20 +0400

    fix userid in particuliers and assos reverse index

--- src/Partage/PartageBundle/Entity/Particulier.php
+++ src/Partage/PartageBundle/Entity/Particulier.php
@@ -40,7 +40,179 @@ class Particulier
     private $objets;
 
     /**
-     * @ORM\OneToOne(targetEntity="User", inversedBy="id")
+     * @ORM\OneToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
      */
     private $user;
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->objets = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Particulier
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Particulier
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return Particulier
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set tel
+     *
+     * @param string $tel
+     *
+     * @return Particulier
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    /**
+     * Get tel
+     *
+     * @return string
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * Add objet
+     *
+     * @param \Partage\PartageBundle\Entity\Objets $objet
+     *
+     * @return Particulier
+     */
+    public function addObjet(\Partage\PartageBundle\Entity\Objets $objet)
+    {
+        $this->objets[] = $objet;
+
+        return $this;
+    }
+
+    /**
+     * Remove objet
+     *
+     * @param \Partage\PartageBundle\Entity\Objets $objet
+     */
+    public function removeObjet(\Partage\PartageBundle\Entity\Objets $objet)
+    {
+        $this->objets->removeElement($objet);
+    }
+
+    /**
+     * Get objets
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getObjets()
+    {
+        return $this->objets;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Partage\PartageBundle\Entity\User $user
+     *
+     * @return Particulier
+     */
+    public function setUser(\Partage\PartageBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Partage\PartageBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
 }
